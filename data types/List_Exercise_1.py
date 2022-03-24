5de597e8ae7299816341ead108362b4ec73d4787
Gopinathtm <102251098+Gopinathtm@users.noreply.github.com>
2022-03-24 20:59:04 +0530

    Update List_Exercise_1.py
    Output updated

--- data types/List_Exercise_1.py
+++ data types/List_Exercise_1.py
@@ -1,4 +1,4 @@
-#python crash course page 47
+#for loop and if statements in list
 
 Dinner_guests_list = ['GOPI', 'SRI', 'KARTHI', 'SUBIN', 'MANI', 'GOVARDHAN', 'ROHAN', 'VIJAY']
 for guest in Dinner_guests_list:
@@ -14,5 +14,31 @@ print()
 Dinner_guests_list = Dinner_guests_list + new_guest
 for table in Dinner_guests_list:
     if len(Dinner_guests_list) > 2:
-        print(f"Sorry no table available Mr.{table}")
-   
+        print(f"Sorry no table available Mr.{table}"
+#output
+              
+"""
+GOPI welcome to the dinner
+SRI welcome to the dinner
+KARTHI welcome to the dinner
+SUBIN welcome to the dinner
+MANI welcome to the dinner
+GOVARDHAN welcome to the dinner
+ROHAN welcome to the dinner
+VIJAY welcome to the dinner
+
+GOVARDHAN could not come
+
+Extra tables available please invite more guests
+New guests invited names are mentioned below
+
+Welcome PAWAR for dinner
+Welcome RAUT for dinner
+Welcome AVIRAJ for dinner
+Welcome SRINIWAS for dinner
+Welcome VARKEES for dinner
+
+Sorry no table available Mr.RAUT
+Sorry no table available Mr.AVIRAJ
+Sorry no table available Mr.SRINIWAS
+Sorry no table available Mr.VARKEES  """           
