b9d44d7030bd3782ad95250c8026841bd01e67fe
Gopinathtm <102251098+Gopinathtm@users.noreply.github.com>
2022-03-24 22:43:02 +0530

    Create While_exercise.py

---
+++ While&input/While_exercise.py
@@ -0,0 +1,25 @@
+#While loop with flag Exercise
+responses = {}
+polling_active = True
+while polling_active:
+    name = input('Enter your name : ')
+    mountain = input('Which mountain you would like to climb : ')
+    responses[name] = mountain
+    repeat = input('would you like to add someone yes/no')
+    if repeat == 'no':
+        polling_active = False
+print('\n Poll results')
+for name, mountain in responses.items():
+    print(f"{name} would like to climb {mountain}")
+#output
+"""
+Enter your name : gopi
+Which mountain you would like to climb : everest
+would you like to add someone yes/noyes
+Enter your name : subin
+Which mountain you would like to climb : abu
+would you like to add someone yes/nono
+ Poll results
+gopi would like to climb everest
+subin would like to climb abu
+"""
