08ac65fce3bcfdbee2e5274bb894275832b643be
Gopinathtm <102251098+Gopinathtm@users.noreply.github.com>
2022-04-13 21:06:02 +0530

    Update class_and_instance_variable.py

--- OOPS/class_and_instance_variable.py
+++ OOPS/class_and_instance_variable.py
@@ -1,40 +1,57 @@
-# class and instance variable defined and called
+# Example of instance variable and class variable
+
 class Employee:
+    # Defined class variable
     num_of_employees = 0
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
+        # Defined instance variables
         self.first = first
         self.last = last
-        self.pay = pay
-        self.email = self.first+'.'+self.last+"@company.com"
+        self.pay = int(pay)  # converted string to int to do calculations
+        self.email = self.first + '.' + self.last + "@company.com"
+        # Class variable incremented as the new object created it gets incremented
         Employee.num_of_employees += 1
 
     def fullname(self):
+        # instance function to get fullname
         return f"{self.first} {self.last}"
 
     def apply_raise(self):
+        # class variable used as intance variable to make changes only to this particular function
         self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        # Class method defined to work with class variable
+        cls.raise_amount = amount
 
 
 emp1 = Employee('Gopi', "TM", '50000')
 emp2 = Employee('Subin', "Varkees", '60000')
 
-
-emp1.raise_amount = 1.05
-print(emp1.__dict__)
-print(emp2.__dict__)
+# Class method argument passed
+Employee.set_raise_amount(1.07)
+# self passed as argument in class to understand self argument
+print(Employee.fullname(emp1))
+print(emp1.fullname())
 print(emp1.raise_amount)
 print(emp2.raise_amount)
-print(emp2.num_of_employees)
+emp1.apply_raise()
+print(emp1.pay)
 
+# changed class variable to specific method
+emp1.raise_amount = 1.08
+# to see all the variables of the instances
+print(emp1.__dict__)
 
 """
 Output
-{'first': 'Gopi', 'last': 'TM', 'pay': '50000', 'email': 'Gopi.TM@company.com', 'raise_amount': 1.05}
-{'first': 'Subin', 'last': 'Varkees', 'pay': '60000', 'email': 'Subin.Varkees@company.com'}
-1.05
-1.04
-2
-
+Gopi TM
+Gopi TM
+1.07
+1.07
+53500
+{'first': 'Gopi', 'last': 'TM', 'pay': 53500, 'email': 'Gopi.TM@company.com', 'raise_amount': 1.08}
 """
